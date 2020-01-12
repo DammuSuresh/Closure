aab60389ed44212b2e3853af5fdf58e8963e40e5
DammuSuresh <40716950+DammuSuresh@users.noreply.github.com>
2020-01-12 18:46:53 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+function greeter(name, age) {
+
+    var message = name + " says hello!! He is " + age + " years old";
+
+    return function greet() {
+
+        console.log(message);
+
+    };
+
+}
+var TestGreeter = greeter("Vinod", 28);
+
+TestGreeter();
+
+//Output //Vinod says hello!! He is 28 years old
